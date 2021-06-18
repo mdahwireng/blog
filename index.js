225c320354805c6b4310f3a791fc546d77647ec3
M D Ahwireng <kaaymike@hotmail.co.uk>
2021-06-18 11:47:49 +0000

    Adds a router

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const fileUpload = require('express-fileupload');
-const controller = require('./controller/controller');
-const middleware = require('./middleware/middleware');
 const expressSession = require('express-session');
 const flash = require('connect-flash');
+const router = require('./route/router');
 
 mongoose.connect('mongodb://localhost/my_database', { useNewUrlParser: true });
 app = new express();
@@ -32,24 +31,4 @@ app.listen(4000, () => {
     console.log(`App is running on http://localhost:4000`);
 });
 
-app.get('/', controller.home);
-
-app.get('/post/:id', controller.getPost);
-
-app.get('/posts/new', middleware.authMiddleware, controller.newPost);
-
-app.post('/posts/store', middleware.authMiddleware, controller.storePost);
-
-app.get('/auth/register', middleware.redirectIfAuthenticatedMiddleware, controller.newUser);
-
-app.post('/users/register', middleware.redirectIfAuthenticatedMiddleware, controller.storeUser);
-
-app.get('/auth/login', middleware.redirectIfAuthenticatedMiddleware, controller.login);
-
-app.get('/auth/logout', controller.logout);
-
-app.post('/users/login', middleware.redirectIfAuthenticatedMiddleware, controller.loginUser);
-
-app.use((req, res) => {
-    res.render('notfound');
-});
+app.use('/', router);
